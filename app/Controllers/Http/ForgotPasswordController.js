551c9d411601a960361d062facf6423142dc5843
Andrey Elyan <ilegra.andrey@agibank.com.br>
2020-10-12 01:52:29 -0300

    initial commit :tada:

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,16 +1,73 @@
 'use strict'
 
+const moment = use('moment')
+
 const User = use('App/Models/User')
+const crypto = require('crypto')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
-  async store ({ request }) {
-    const email = request.input('email')
-    const user = await User.findBy('email', email)
+  async store ({ request, response }) {
+    try {
+      const email = request.input('email')
+      const user = await User.findByOrFail('email', email)
 
-    user.token = '123123'
-    user.token_created_at = new Date()
+      user.token = crypto.randomBytes(10).toString('hex')
+      user.token_created_at = new Date()
 
-    await user.save()
+      await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        { email, token: user.token, link: `${request.input('redirect_url')}` },
+        message => {
+          message
+            .to(user.email)
+            .from('andrey.silveira@ilegra.com', 'Andrey Elyan | Ilegra')
+            .subject('Recuperação de senha')
+        }
+      )
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({ error: { message: 'Algo não deu certo, esse email existe?' } })
+    }
+  }
+
+  async update ({ request, response }) {
+    try {
+      const { token, password } = request.all()
+
+      const user = await User.findByOrFail('token', token)
+
+      const tokenExpired = moment()
+        .subtract('2', 'days')
+        .isAfter(user.token_created_at)
+
+      if (tokenExpired) {
+        return response
+          .status(401)
+          .send({
+            error: {
+              message: 'o Token de recuperação está expirado'
+            }
+          })
+      }
+
+      user.token = null
+      user.token_created_at = null
+      user.password = password
+
+      await user.save()
+    } catch (err) {
+      return response
+        .status(err.status)
+        .send({
+          error: {
+            message: 'Algo deu errado ao resetar sua senha'
+          }
+        })
+    }
   }
 }
 
